cdf01fc89d1aaa6439b3d64e04d4d0e818d830dc
david.ziegler <david.ziegler@cc6676e4-1e4b-11de-8b5c-13accb87f508>
2009-04-06 05:11:42 +0000

    applied patch to resolve issue 2: rename compress imports to django-css

--- django_css/filters/csstidy_python/__init__.py
+++ django_css/filters/csstidy_python/__init__.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 
-from compress.filter_base import FilterBase
-from compress.filters.csstidy_python.csstidy import CSSTidy
+from django_css.filter_base import FilterBase
+from django_css.filters.csstidy_python.csstidy import CSSTidy
 
 COMPRESS_CSSTIDY_SETTINGS = getattr(settings, 'COMPRESS_CSSTIDY_SETTINGS', {})
 
